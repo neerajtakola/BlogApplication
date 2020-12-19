c8770876de0824316062891f3a900a8e07f07757
neeraj.mountblue <neeraj.takola@mountblue.tech>
2020-12-19 20:39:35 +0530

    Added query in IPostRepository

--- src/main/java/com/app/blogapplication/dao/IPostRepository.java
+++ src/main/java/com/app/blogapplication/dao/IPostRepository.java
@@ -1,13 +1,11 @@
 package com.app.blogapplication.dao;
 
 import com.app.blogapplication.entities.Post;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface IPostRepository extends JpaRepository<Post,Integer> {
